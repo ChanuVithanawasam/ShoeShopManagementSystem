ab00a941bed2f4e8ed3855e9fd6df9c6a7434376
ChAnu VithAnawasam <chanumadhuprasanna99@gmail.com>
2024-04-22 00:58:56 -0700

    creating project structure and adding dependencies

---
+++ HelloShoes/src/test/java/src/main/com/helloShoes/HelloShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package src.main.com.helloShoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
