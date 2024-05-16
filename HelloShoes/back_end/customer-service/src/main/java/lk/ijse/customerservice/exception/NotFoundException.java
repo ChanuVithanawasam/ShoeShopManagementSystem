4954a03e4d567068455c1795f0df83ec04ff5d31
ChAnu VithAnawasam <chanumadhuprasanna99@gmail.com>
2024-05-15 23:10:58 -0700

    adding back end

--- HelloShoes/back_end/customer-service/src/main/java/lk/ijse/customerservice/exception/NotFoundException.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.customerservice.exception;
-public class NotFoundException extends RuntimeException{
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
--- HelloShoes/back_end/customer-service/src/main/java/lk/ijse/customerservice/exception/ValidationException.java
+++
@@ -1,6 +0,0 @@
-package lk.ijse.customerservice.exception;
-public class ValidationException extends RuntimeException{
-    public ValidationException(String message) {
-        super(message);
-    }
-}
